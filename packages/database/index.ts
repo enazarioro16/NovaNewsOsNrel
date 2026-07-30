0764abb1685e6f35116686178bf8c354083cb62f
ggooooggllee <roaedinson60@gmail.com>
2026-07-29 22:20:47 -0500

    hotfix: remove old root index.ts and map src to dist in database package

--- packages/database/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-
-// Placeholder Connection for Foundation Sprint
-const queryClient = postgres('postgres://novanews:novanews_password@localhost:5432/novanews_db');
-export const db = drizzle(queryClient);
