5489400749a331ee12ac11d213be7d37dd174708
ggooooggllee <roaedinson60@gmail.com>
2026-07-30 11:26:04 -0500

    fix(api): bind nestjs to 0.0.0.0 to fix docker network routing

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-  console.log('API Gateway Core running on http://localhost:3001');
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
+  console.log(`API Gateway Core running on http://0.0.0.0:${process.env.PORT || 3001}`);
 }
 bootstrap();
